bd1d8638f8daa01311a61515d1102a7b060051ae
Daniel Lupiañez Casares <lupidan@gmail.com>
2012-05-08 00:06:46 +0200

    Bug solving and a lot of fixes. Added example.
    Added table view example.
    Added category for NSData to create MD5 Digests
    Fixed discovered bugs when several UILazyImageView are on screen

---
+++ UILazyImageView/UILazyImageView/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  UILazyImageView
+//
+//  Created by Daniel Lupiañez Casares on 07/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
